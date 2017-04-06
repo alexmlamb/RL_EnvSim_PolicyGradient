69673ed9975ef56e80731c26b5eff7ac822806e1
Nan Rosemary Ke <rosemary.ke@west.cmu.edu>
2017-04-06 18:36:06 -0400

    experiment.py

--- experiment.py
+++ experiment.py
@@ -13,8 +13,17 @@ env = gym.make('Pendulum-v0')
 env.reset()
 
 for i_episode in range(10000):
-    env.render()
-    env.step(env.action_space.sample())
+    observ = env.reset()
+    for t in range(100):
+        env.render()
+        print observ
+        import ipdb; ipdb.set_trace()
+        action = env.action_space.sample()
+        #env.step(env.action_space.sample())
+        observ, reward, done, info = env.step(action)
+        if done:
+            print 'episode finished after {} steps'.format(t+1)
+            break
 
 
 
