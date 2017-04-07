c4c3921fb0519350ac866f731c39c3f9cc43f823
Nan Rosemary Ke <rosemary.ke@west.cmu.edu>
2017-04-06 22:29:26 -0400

    real chain

--- lib/main.py
+++ lib/main.py
@@ -12,6 +12,13 @@ import theano.tensor as T
 from nn_layers import fflayer, param_init_fflayer
 from utils import init_tparams, join2, srng, dropout, inverse_sigmoid
 
+from collections import OrderedDict
+import gym
+import logging
+import cPickle
+import numpy as np
+import argparse
+
 action_size = 1
 state_size = 1
 reward_size = 1
@@ -81,18 +88,27 @@ def simulated_chain(params_envsim, params_policy, initial_state):
     reward_lst = []
     return reward_lst
 
-params_policy = init_params_policy({})
-params_envsim = init_params_envsim({})
+params_policy = OrderedDict()
+params_envsim  = OrderedDict()
 
-state = T.matrix()
-action = T.matrix()
+params_policy = init_params_policy({params_policy})
+params_envsim = init_params_envsim({params_envsim})
+
+tstate = T.matrix()
+taction = T.matrix()
+
 
 next_action = policy_network(params_policy, state)
 
 next_state, reward = envsim_network(params_envsim, state, action)
 
+env = gym.make('Pendulum-v0')
+state_value = env.reset()
+
+compute_action = theano.function([state, tparams_policy],[next_action])
 
 
-
-
-
+for i_episode in range(100):
+    # sample action from the policy network
+    # pass the action to the simulator network
+    action_ = compute_action(state_value, params_policy)
