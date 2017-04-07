4d66123014048b2f98d8d7adc51b05609abbc43c
Alexander Lamb <lambalex@leto07.iro.umontreal.ca>
2017-04-06 20:12:08 -0400

    updates

--- lib/main.py
+++ lib/main.py
@@ -11,12 +11,17 @@ import theano
 import theano.tensor as T
 from nn_layers import fflayer, param_init_fflayer
 from utils import init_tparams, join2, srng, dropout, inverse_sigmoid
+import lasagne
+import numpy.random as rng
+import numpy as np
 
 action_size = 1
 state_size = 1
 reward_size = 1
 nfp = 512
 nfe = 512
+num_steps_simulated_chain = 20
+mb = 64
 
 #state -> action
 def init_params_policy(p):
@@ -75,14 +80,35 @@ def real_chain(params_policy, initial_state):
 
     return action_lst, state_lst, reward_lst
 
-def simulated_chain(params_envsim, params_policy, initial_state):
+def net_simulated_chain(params_envsim, params_policy, initial_state, num_steps):
 
 
-    reward_lst = []
+    initial_reward = theano.shared(np.zeros(shape=(mb,1)).astype('float32'))
+
+    def one_step(last_state,last_reward):
+
+        next_action = policy_network(params_policy, initial_state)
+        next_state, next_reward = envsim_network(params_envsim, last_state, next_action)
+        
+        return next_state, next_reward
+
+    [state_lst, reward_lst], _ = theano.scan(fn=one_step,outputs_info=[initial_state, initial_reward],n_steps=num_steps)
+
     return reward_lst
 
 params_policy = init_params_policy({})
 params_envsim = init_params_envsim({})
+
+initial_state_sim = T.matrix()
+
+simulated_reward_lst = net_simulated_chain(params_envsim, params_policy, initial_state_sim,num_steps_simulated_chain)
+
+simulated_total_reward = T.sum(simulated_reward_lst)
+simulation_loss = -1.0 * simulated_total_reward
+
+simulation_updates = lasagne.updates.adam(simulation_loss, params_policy.values())
+
+simulation_function = theano.function(inputs=[initial_state_sim],outputs=[simulated_total_reward],updates=simulation_updates)
 
 state = T.matrix()
 action = T.matrix()
@@ -91,8 +117,13 @@ next_action = policy_network(params_policy, state)
 
 next_state, reward = envsim_network(params_envsim, state, action)
 
+for iteration in range(0,50000): 
 
+    initial_state_sim = rng.normal(size = (64, state_size)).astype('float32')
 
+    simulated_reward = simulation_function(initial_state_sim)
 
+    if iteration % 500 == 1:
+        print iteration, simulated_reward
 
 
